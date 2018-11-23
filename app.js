617d690225baa5c18c4993cfa5f07e46994189d9
Dev Say <game@devsay.com>
2018-11-22 22:00:12 -0300

    k

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const morgan = require('morgan')
 const cron = require('node-cron')
 
-require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
